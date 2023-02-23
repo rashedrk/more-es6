56829f5e8581efd31035ce02c7c4845339fffe96
rashedrk <rashedrk001@gmail.com>
2023-02-23 22:16:10 +0600

    for each

---
+++ forEach.js
@@ -0,0 +1,8 @@
+const products = [
+    {id: 1, name: 'laptop', price: 84000},
+    {id: 2, name: 'watch', price: 8000},
+    {id: 3, name: 'mobile', price: 78000},
+    {id: 4, name: 'tablet', price: 23000}
+];
+
+products.forEach(product => console.log(product.name));
